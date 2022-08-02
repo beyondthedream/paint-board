9e2f67817098c46270c70a01b095cb44d9a46af1
Yena Kwon <ynkwon@softrain.co.kr>
2022-08-02 19:05:59 +0900

    build: basic paint function added

---
+++ app.js
@@ -0,0 +1,185 @@
+/** @format */
+const saveBtn = document.getElementById("save");
+const textInput = document.getElementById("text");
+const fileInput = document.getElementById("file");
+const lineWidth = document.getElementById("line-width");
+const color = document.getElementById("color");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
+const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraseBtn = document.getElementById("eraser-btn");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
+
+let isPainting = false;
+let isFilling = false;
+
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting() {
+  isPainting = true;
+}
+function cancelPainting() {
+  isPainting = false;
+  ctx.beginPath();
+}
+
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+
+function onModeClick(event) {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+
+function onDestroyClick() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraseClick() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+  modeBtn.innerText = "Fill";
+}
+
+function onFileChange(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  };
+}
+
+function onDoubleClick(event) {
+  const text = textInput.value;
+  if (text !== "") {
+    ctx.save();
+    const text = textInput.value;
+    ctx.lineWidth = 1;
+    ctx.lineWidth = "68px serif";
+    ctx.strokeText(text, event.offsetX, event.offsetY);
+    ctx.restore();
+  }
+}
+
+function onSaveClick(event) {
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "myDrawing.png";
+  a.click();
+}
+
+canvas.addEventListener("dbclick", onDoubleClick);
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
+
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraseBtn.addEventListener("click", onEraseClick);
+
+fileInput.addEventListener("change", onFileChange);
+saveBtn.addEventListener("click", onSaveClick);
+
+// function onClick(event) {
+//   ctx.beginPath();
+//   ctx.moveTo(0, 0);
+//   const color = colors[Math.floor(Math.random() * colors.length)];
+//   ctx.strokeStyle = color;
+//   ctx.lineTo(event.offsetX, event.offsetY);
+//   ctx.stroke();
+// }
+
+// canvas.addEventListener("mousemove", onClick);
+
+// ctx.fillRect(220, 180, 30, 100);
+// ctx.fillRect(340, 180, 30, 100);
+// ctx.fillRect(260, 180, 70, 200);
+// ctx.arc(290, 120, 50, 0, 2 * Math.PI);
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.fillStyle = "yellow";
+// ctx.arc(270, 110, 10, 1 * Math.PI, 2 * Math.PI);
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.fillStyle = "green";
+// ctx.arc(310, 110, 10, 1 * Math.PI, 2 * Math.PI);
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.fillStyle = "red";
+// ctx.arc(290, 125, 30, 0 * Math.PI, 1 * Math.PI);
+// ctx.fill();
+
+// ctx.fillRect(200, 200, 50, 200);
+// ctx.fillRect(400, 200, 50, 200);
+// ctx.lineWidth = 2;
+// ctx.fillRect(300, 300, 50, 100);
+// ctx.fillRect(200, 200, 200, 20);
+// ctx.moveTo(200, 200);
+// ctx.lineTo(325, 100);
+// ctx.lineTo(450, 200);
+// ctx.fill();
+
+// const colors = [
+//   "#ff7675",
+//   "#ff3838",
+//   "#ffb8b8",
+//   "#c56cf0",
+//   "#ff9f1a",
+//   "#fff200",
+//   "#32ff7e",
+//   "#7efff5",
+//   "#18dcff",
+//   "#7d5fff",
+// ];
